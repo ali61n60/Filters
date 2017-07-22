c2b8a5cba124a621485117dc8e1702cfbd5a676d
ali2 <ali62n62@yahoo.com>
2017-07-22 16:02:15 +0430

    page 609

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Filters.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Filters.Controllers
 {
+    [HttpsOnly]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-
-            if (!Request.IsHttps)
-            {
-                return new StatusCodeResult(StatusCodes.Status403Forbidden);
-            }
-            else
-            {
-
-                return View("Message",
+            return View("Message",
                     "This is the Index action on the Home controller " +
                     HttpContext.Connection.RemoteIpAddress + " " +
                     HttpContext.Connection.RemotePort);
-            }
         }
+        public ViewResult SecondAction() => View("Message",
+            "This is the SecondAction action on the Home controller");
     }
+
+    
 }
