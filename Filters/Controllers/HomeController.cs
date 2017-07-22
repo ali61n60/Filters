7a9c1b2ae6465f9c6f6f807739cd5f6ac9220019
Ali <ali62n62@yahoo.com>
2017-07-23 01:22:20 +0430

    p614

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -11,15 +11,12 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Filters.Controllers
 {
-    [HttpsOnly]
+    [Profile]
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View("Message",
-                    "This is the Index action on the Home controller " +
-                    HttpContext.Connection.RemoteIpAddress + " " +
-                    HttpContext.Connection.RemotePort);
+            return View("Message","This is the Index action on the Home controller" );
         }
         public ViewResult SecondAction() => View("Message",
             "This is the SecondAction action on the Home controller");
---
+++ Filters/Infrastructure/ProfileAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace Filters.Infrastructure
+{
+    public class ProfileAttribute : ActionFilterAttribute
+    {
+        private Stopwatch timer;
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            timer.Stop();
+            string result = "<div>Elapsed time: "
+                            + $"{timer.Elapsed.TotalMilliseconds} ms</div>";
+            byte[] bytes = Encoding.UTF8.GetBytes(result);
+            context.HttpContext.Response.Body.Write(bytes,0,bytes.Length);
+        }
+    }
+}
